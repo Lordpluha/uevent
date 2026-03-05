ca9edb7d60181428270948ea09407fd3cfd706cd
lordpluha <Tesluakevlad@gmail.com>
2026-03-05 15:08:14 +0100

    Create first CRUD

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './modules/users/entities/user.entity'
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5434,
+      username: 'uevent',
+      password: 'uevent',
+      database: 'uevent',
+      entities: [User],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
